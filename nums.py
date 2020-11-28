e3d0b02c04b65b723b5127765bac5974e23ab23f
ognjengari <ognjengari@gmail.com>
2020-11-28 11:39:06 +0100

    generators introduces

--- nums.py
+++ nums.py
@@ -8,7 +8,7 @@ def gen_nums(n):
 
 
 def return_primes(nums):
-    return [x for x in nums if is_prime(x)]
+    return (x for x in nums if is_prime(x))
 
 
 def is_prime(num):
@@ -26,20 +26,19 @@ def is_prime(num):
 
 
 def return_hex(nums):
-    return [hex(x) for x in nums]
+    return (hex(x) for x in nums)
 
 
 def reformat_hex(nums):
-    return [x[2:] for x in nums]
+    return (x[2:] for x in nums)
 
 
 def nums_from_hex(hex_nums):
     nums = []
 
-    temp_nums = [re.findall("[0-9]", x) for x in hex_nums]
+    temp_nums = (re.findall("[0-9a-zA-Z]", x) for x in hex_nums)
     [nums.extend(el) for el in temp_nums]
     return Counter(nums)
-
 
 
 def main():
