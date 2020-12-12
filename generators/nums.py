385f199c047271901fd1f01d96b973b2db4339b6
Ognjen <ognjengari@gmail.com>
2020-12-12 13:52:42 +0100

    multiprocessing use

--- generators/nums.py
+++ generators/nums.py
@@ -1,15 +1,11 @@
 import math
 import re
-import tracemalloc
 from collections import Counter
+from multiprocessing import Pool
 
 
 def gen_nums(n):
     return (x for x in range(1, n))
-
-
-def return_primes(nums):
-    return (x for x in nums if is_prime(x))
 
 
 def is_prime(num):
@@ -18,43 +14,47 @@ def is_prime(num):
         for i in range(2, math.ceil(math.sqrt(num)) + 1):
 
             if (num % i) == 0:
-                return False
+                continue
         else:
-            return True
+            return return_hex(num)
 
     else:
-        return True
+        return return_hex(num)
 
 
-def return_hex(nums):
-    return (hex(x) for x in nums)
+def return_hex(num):
+    hex_num = hex(num)
+    return reformat_hex(hex_num)
 
 
-def reformat_hex(nums):
-    return (x[2:] for x in nums)
+def reformat_hex(num):
+    temp = num[2:]
+    return nums_from_hex(temp)
 
 
-def nums_from_hex(hex_nums):
+def nums_from_hex(hex_num):
+    num = re.findall("[0-9a-zA-Z]", hex_num)
+    return num
+
+
+def append_arrays(arr):
     nums = []
-
-    temp_nums = (re.findall("[0-9a-zA-Z]", x) for x in hex_nums)
-    [nums.extend(el) for el in temp_nums]
-    return Counter(nums)
+    [nums.extend(x) for x in arr]
+    return nums
 
 
 def main():
-
     x = 1000000
     x = gen_nums(x)
-    x = return_primes(x)
-    x = return_hex(x)
-    x = reformat_hex(x)
-    x = nums_from_hex(x)
 
-    print(x)
+    with Pool() as pool:
+        nums = pool.map(is_prime, x)
+
+    nums = append_arrays(nums)
+    nums = Counter(nums)
+
+    print(nums)
 
 
 if __name__ == "__main__":
     main()
-
-
